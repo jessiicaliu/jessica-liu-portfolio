ceb6a98334edd364504f0c4fb46bee23112bec82
Jessica Liu <j2357liu@uwaterloo.ca>
2026-04-08 13:57:51 -0400

    set up React Router and add 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
-export default function App() {
-  return <div>App</div>;
-}
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NotFound from "./pages/NotFound";
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<div>Coming soon</div>} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App;
